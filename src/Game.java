83c94643f1d8b33eb0d9cc2ddc28f4b169b943d6
dadajuice <davidt2003@msn.com>
2022-09-07 10:18:50 -0400

    Refactor: encapsulate grid into new class

--- src/Game.java
+++ src/Game.java
@@ -2,71 +2,36 @@ import java.util.Random;
 
 public class Game {
 
-    private final int PLAYER = 0;
-    private final int AI = -1;
-    private int[][] gameGrid;
+    private Grid grid;
     private boolean playerWon = false;
     private boolean computerWon = false;
     private boolean isGridFilled;
 
     public Game() {
-        initializeGrid();
-        displayGrid();
+        grid = new Grid();
     }
 
     public void startGame() {
         do {
             playerTurn();
-            playerWon = isTitleholder(PLAYER);
+            playerWon = isTitleholder(Grid.PLAYER);
             if (!playerWon) {
                 computerTurn();
-                computerWon = isTitleholder(AI);
+                computerWon = isTitleholder(Grid.AI);
             }
-            displayGrid();
-            isGridFilled = isFull();
+            grid.display();
+            isGridFilled = grid.isFull();
         }  while (!playerWon && !isGridFilled && !computerWon);
         gameEnded();
     }
 
-    public void displayGrid() {
-        Terminal.lineBreak();
-
-        for (int j = 0; j < 3; ++j) {
-            for (int i = 0; i < 3; ++i) {
-                if (gameGrid[j][i] == PLAYER) {
-                    Terminal.message(" X ");
-                } else if (gameGrid[j][i] == AI) {
-                    Terminal.message(" O ");
-                } else {
-                    Terminal.message(" "+ gameGrid[j][i] +" ");
-                }
-                if (i != 2) {
-                    Terminal.message("|");
-                }
-            }
-            Terminal.lineBreak();
-        }
-    }
-
     public boolean isTitleholder(int playerValue) {
-        TitleholderManeuverer maneuverer = new TitleholderManeuverer(gameGrid);
-        return maneuverer.isTitleholder(playerValue);
-    }
-
-    public void initializeGrid() {
-        this.gameGrid = new int[3][3];
-        int gridInitializer = 1;
-        for (int j = 0; j < 3; ++j) {
-            for (int i = 0; i < 3; ++i) {
-                gameGrid[j][i] = gridInitializer;
-                ++gridInitializer;
-            }
-        }
+        return grid.isTitleholder(playerValue);
     }
 
     public void playerTurn() {
         int coord = askCoordinate();
-        gameGrid[(coord - 1) / 3][(coord - 1) % 3] = PLAYER;
+        grid.placeIndicator((coord - 1) / 3, (coord - 1) % 3, Grid.PLAYER);
     }
 
     private int askCoordinate() {
@@ -95,25 +60,13 @@ public class Game {
             xPosition = randomGenerator.nextInt(3);
             yPosition = randomGenerator.nextInt(3);
 
-            if (gameGrid[xPosition][yPosition] == PLAYER || gameGrid[xPosition][yPosition] == AI){
+            if (grid.indicatorExists(xPosition, yPosition)){
                 valid = false;
             } else {
-                gameGrid[xPosition][yPosition] = AI;
+                grid.placeIndicator(xPosition, yPosition, Grid.AI);
                 valid = true;
             }
         } while (!valid);
-    }
-
-    public boolean isFull()  {
-        int fullCellCount = 0;
-        for (int j = 0; j < 3; ++j) {
-            for (int i = 0; i < 3; ++i) {
-                if (gameGrid[j][i] != PLAYER && gameGrid[j][i] != AI) {
-                    ++fullCellCount;
-                }
-            }
-        }
-        return fullCellCount <= 0;
     }
 
     private void gameEnded() {
---
+++ src/Grid.java
@@ -0,0 +1,67 @@
+public class Grid {
+
+    public static final int PLAYER = 0;
+    public static final int AI = -1;
+
+    private int[][] gameGrid;
+
+    public Grid() {
+        initialize();
+    }
+
+    public void placeIndicator(int x, int y, int value) {
+        gameGrid[x][y] = value;
+    }
+
+    public boolean indicatorExists(int x, int y) {
+        return gameGrid[x][y] == PLAYER || gameGrid[x][y] == AI;
+    }
+
+    public boolean isTitleholder(int playerValue) {
+        TitleholderManeuverer maneuverer = new TitleholderManeuverer(gameGrid);
+        return maneuverer.isTitleholder(playerValue);
+    }
+
+    public boolean isFull()  {
+        int fullCellCount = 0;
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; ++i) {
+                if (gameGrid[j][i] != PLAYER && gameGrid[j][i] != AI) {
+                    ++fullCellCount;
+                }
+            }
+        }
+        return fullCellCount <= 0;
+    }
+
+    public void display() {
+        Terminal.lineBreak();
+
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; ++i) {
+                if (gameGrid[j][i] == PLAYER) {
+                    Terminal.message(" X ");
+                } else if (gameGrid[j][i] == AI) {
+                    Terminal.message(" O ");
+                } else {
+                    Terminal.message(" "+ gameGrid[j][i] +" ");
+                }
+                if (i != 2) {
+                    Terminal.message("|");
+                }
+            }
+            Terminal.lineBreak();
+        }
+    }
+
+    private void initialize() {
+        this.gameGrid = new int[3][3];
+        int gridInitializer = 1;
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; ++i) {
+                gameGrid[j][i] = gridInitializer;
+                ++gridInitializer;
+            }
+        }
+    }
+}
